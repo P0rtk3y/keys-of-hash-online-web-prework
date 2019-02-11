a1856ec66569f5c5fc08f4b6d49a5faf4d229218
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-11 05:08:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,14 +2,15 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    values.each do |v| 
     key_array = []
+    
+    values.each do |v| 
       if v == arguments.join 
         index = values.index(v)
         key_array << keys.at(index) 
       end 
-      key_array
     end 
+    key_array
   end
 end
 
