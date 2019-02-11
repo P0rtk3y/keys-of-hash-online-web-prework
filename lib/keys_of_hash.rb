fcd1b6cad6850b0e2d459de1821858404c703749
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-11 05:05:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,4 +7,10 @@ class Hash
   end
 end
 
- 
+ values.each do |v| 
+      if v == arguments.join 
+        index = values.index(v)
+        key_array << keys.at(index) 
+      end 
+    end 
+    puts key_array
