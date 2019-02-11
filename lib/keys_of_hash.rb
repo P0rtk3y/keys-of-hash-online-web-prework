33d570e4fe08b7692da1055bf29d1958cac55dbd
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-11 05:25:10 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,14 +3,12 @@ require 'pry'
 class Hash
  
   def keys_of(*arguments)
-   
     key_array = []
     arguments.each do |country| 
       self.each do |k, v|
         if v == country 
           key_array << k
         end 
-        
       end 
     end 
     key_array
