69008829e47465dcbea58c66ebab281c14fc318e
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-11 05:39:33 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,24 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    key_array = []
+    arguments.each do |country| 
+      self.each do |k, v|
+        if v == country 
+          key_array << k
+        end 
+      end 
+    end 
+    key_array
   end
 end
+
+
+# key_array = []
+# values.each do |v|
+#   if v == arguments 
+#     index = values.index(v)
+#     key_array << keys.at(index)
+#   end 
+# end 
